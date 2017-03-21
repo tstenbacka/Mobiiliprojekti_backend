75bafe7c55e25427656d86c7bca6847a1a542b5c
tstenbacka <tomi.stenbacka@gmail.com>
2017-03-21 14:15:56 +0200

    Created initial routing
    Initial placeholder data and routing

--- mobiili_backend/mobiili_backend/mobiili_backend.py
+++ mobiili_backend/mobiili_backend/mobiili_backend.py
@@ -5,10 +5,76 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+        return "index"
+
+@app.route('/users/<id>')
+def users(id):
         return jsonify(
-        temperature=random.randint(-35, 40) ,
-	humidity=random.randint(5, 90)
-    )
+        id = "%s" %id ,
+        user_name = "test" ,
+        first_name = "test" ,
+        last_name = "test" ,
+        email = "test@test.lol"
+        )
+
+@app.route('/users', methods=['POST'])
+def signup():
+        return request.json
+
+@app.route('/foods', methods=['GET', 'POST'])
+def foods():
+        foods = [
+            {
+                'id' : 1 ,
+                'IMG_URL' : "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933",
+                'food_name' : "hernari" ,
+                'description' : "hernari"
+             },
+             {
+                'id' : 2 ,
+                'IMG_URL' : "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933",
+                'food_name' : "gorilla" ,
+                'description' : "gorilla lihabullia"
+             }
+        ]
+        
+        return jsonify(foods)
+
+@app.route('/foods/<id>')
+def food(id):
+        return jsonify(
+        id = "%s" %id ,
+        IMG_URL = "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933" ,
+        food_name = "hernari" ,
+        description = "hernari"
+        )
+
+@app.route('/drinks')
+def drinks():
+        drinks = [
+            {
+                'id' : 1 ,
+                'IMG_URL' : "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933" ,
+                'drink_name' : "santtu" ,
+                'description' : "Suomen yliarvostetuin olut"
+            } ,
+            {
+                'id' : 2 ,
+                'IMG_URL' : "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933" ,
+                'drink_name' : "Koff" ,
+                'description' : "Jumalten juoma"              
+            }
+        ]
+        return jsonify(drinks)
+
+@app.route('/drinks/<id>')
+def drink(id):
+        return jsonify(
+        id = "%s" %id ,
+        IMG_URL = "http://vignette3.wikia.nocookie.net/meme/images/c/c7/Fd665178b5.jpg/revision/latest?cb=20160524214933" ,
+        drink_name = "santtu" ,
+        description = "Suomen yliarvostetuin olut"
+        )
 
 if __name__ == "__main__":
     app.run()
