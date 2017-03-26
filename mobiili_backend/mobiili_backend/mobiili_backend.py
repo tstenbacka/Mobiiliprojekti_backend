a44392cdbb78189aeaf0ccfa7e4eade0c939da18
armageddom <jere.kylatie@hotmail.com>
2017-03-26 02:41:02 +0200

    POST users ei testattu
    ei pyg testaa ennenku ville comittaa frontin. GG

--- mobiili_backend/mobiili_backend/mobiili_backend.py
+++ mobiili_backend/mobiili_backend/mobiili_backend.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import jsonify
 import MySQLdb
+import requests
 
 app = Flask(__name__)
 
 # add here the actual DB info
 # db = MySQLdb.connect("127.0.0.1", "root", "asd", "Mobiili")
-db = MySQLdb.connect("127.0.0.1", "root", "asd", "Mobiili")
+db = MySQLdb.connect("127.0.0.1", "root", "mobiili", "Mobiili")
 
 @app.route('/')
 def index():
@@ -40,7 +41,31 @@ def users(id):
         # db.close()
 @app.route('/users', methods=['POST'])
 def signup():
+		cursor = db.cursor()
+		data = request.json
+        firstname = request.json['Firstname']
+        lastname = request.json['Lastname']
+        username = request.json['Username']
+        email = request.json['Email']
+        password = request.json['Password']
+        response.content_type = 'application/json'
+        if not data:
+        	abort(400, 'No data received')
+
+
+		sql = "INSERT INTO User (Firstname, Lastname, Username, Email, Password ) VALUES ('&s','&s'&s') "
+	    try:
+	    # Execute dml and commit changes
+	        cursor.execute(sql,data)
+	        db.commit()
+	        cursor.close()        
+	    except:
+	    # Rollback changes
+	        db.rollback()
+	    return dumps(("OK"),default=json_util.default)			
         return request.json
+
+def login():        
 
 @app.route('/foods', methods=['GET', 'POST'])
 def foods():
